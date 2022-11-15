fdc1c203fbae100a1ad40fb46194c250e4fb10b5
hojat <hojat72elect@gmail.com>
2022-11-15 03:01:15 -0500

    Implemented an Echo server

--- src/main.py
+++ src/main.py
@@ -1,1 +1,18 @@
-print("Hack World!")
+import socket
+
+# a simple echo - server
+
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as defaultSocket:
+    defaultSocket.bind((HOST, PORT))
+    defaultSocket.listen()
+    connection, address = defaultSocket.accept()
+    print(f"The address of the connection is {address}")
+
+    while True:
+        data = connection.recv(1024)
+        if not data:
+            break
+        connection.sendall(data)
