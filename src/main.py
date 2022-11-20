a01ec301533d856ea93f449d74e8b5b57f3c44b7
hojat <hojat72elect@gmail.com>
2022-11-20 03:06:09 -0500

    some renamings

--- src/main.py
+++ src/main.py
@@ -5,10 +5,10 @@ import socket
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as defaultSocket:
-    defaultSocket.bind((HOST, PORT))
-    defaultSocket.listen()
-    connection, address = defaultSocket.accept()
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as default_socket:
+    default_socket.bind((HOST, PORT))
+    default_socket.listen()
+    connection, address = default_socket.accept()
     print(f"The address of the connection is {address}")
 
     while True:
