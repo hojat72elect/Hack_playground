74aee0cf732eb81fe94dd05d294a885ca7a3aa38
hojat <hojat72elect@gmail.com>
2022-11-23 19:19:46 -0500

    convert_address_to_ip

---
+++ src/socket/convert_address_to_ip.py
@@ -0,0 +1,12 @@
+import socket
+
+"""
+convert a website's address to IP address. 
+"""
+
+# The address of the website
+website_address = 'www.leader.ir'
+
+# The IP address of the website
+ip_address = socket.gethostbyname(website_address)
+print("Website's IP address:", ip_address)
