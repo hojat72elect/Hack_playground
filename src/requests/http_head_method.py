8248c59bff4cd432871537f8e310208c9164a757
hojat <hojat72elect@gmail.com>
2022-11-26 23:52:25 -0500

    updated HTTP head calls

--- src/requests/http_head_method.py
+++ src/requests/http_head_method.py
@@ -7,12 +7,17 @@ requested with the HTTP GET method. For example, if a URL might produce a large 
 could read its Content-Length header to check the filesize without actually downloading the file.
 
 As you can see below, the header will give lots of important info about the HTTP server.
-
 """
+http_prefix = "http://"
 
-http_result: requests.Response = requests.head(" http://" + penetration_targets.makarem_shirazi)
-result_dictionary = dict(http_result.headers)
-print(result_dictionary)
-print("HTTP server: ", result_dictionary['Server'])
-print("Cache-Control: ", result_dictionary['Cache-Control'])
-print("Content-Encoding: ", result_dictionary['Content-Encoding'])
+header_first: requests.Response = requests.head(http_prefix + penetration_targets.makarem_shirazi)
+result_dictionary_first = dict(header_first.headers)
+print(result_dictionary_first)
+print("HTTP server: ", result_dictionary_first['Server'])
+print("Cache-Control: ", result_dictionary_first['Cache-Control'])
+print("Content-Encoding: ", result_dictionary_first['Content-Encoding'])
+
+# Let's get the header of another target and compare that with this one:
+header_second: requests.Response = requests.head(http_prefix + penetration_targets.irna)
+result_dictionary_second = dict(header_second.headers)
+print(result_dictionary_second)
